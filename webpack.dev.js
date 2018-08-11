ab7d693632b73ed4ac10c6fc418c707eff56a8f8
ryakosh <ryakosh@gmail.com>
2018-08-11 14:44:53 +0430

    initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const common = require('./webpack.common.js');
+const merge = require('webpack-merge');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+});
---
+++ webpack.prod.js
@@ -0,0 +1,26 @@
+const common = require('./webpack.common.js');
+const merge = require('webpack-merge');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = merge(common, {
+  mode: 'production',
+  plugins: [],
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': 'production',
+    }),
+  ],
+});
